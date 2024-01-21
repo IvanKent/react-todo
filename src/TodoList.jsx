c3b44dd8eaefba47faf9d49f675f0f406a470f7b
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-01-21 11:09:26 +0800

    added a function to toggle the checkbox

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -25,10 +25,25 @@ export default function TodoList() {
               return prevTodos.filter(todo => todo.id !== id); 
         })
     }
+
+    const toggleTodo = (id) => {
+        setTodos(prevTodos => {
+            return prevTodos.map(todo => {
+                if(todo.id === id){
+                    return { ...todo, completed: !todo.completed}
+                }else{
+                    return todo;
+                }
+            })
+        })
+    }
     return (
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
         {todos.map((todo) => {
-            return <TodoItem todo={todo} key={todo.id} removeTodo={() => (removeTodo(todo.id))}/>
+            return <TodoItem todo={todo} key={todo.id} removeTodo={() => (removeTodo(todo.id))}
+             toggleTodo={() => {
+                return toggleTodo(todo.id)
+             }}/>
         })}
       </List>
     )
